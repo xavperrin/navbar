fe700ce712ad40020723b00e9b376fc97d8c0f39
xavperrin <xl.perrin@gmail.com>
2023-01-13 10:37:32 +0100

    js toggle menu feature

--- app.js
+++ app.js
@@ -3,3 +3,11 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle=document.querySelector('.nav-toggle');
+const links=document.querySelector('.links');
+
+navToggle.addEventListener('click', ()=>{
+console.log(links.classList);
+links.classList.toggle('show-links');
+})
